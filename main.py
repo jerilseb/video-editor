6961d83d2825255cd3e42a937a86aeee3d45b3ba
Jeril Sebastian <jeril.sebastian@gmail.com>
2025-01-09 08:02:46 +0000

    some fixes

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import gradio as gr
 from process_video import process_video
 
-# Create the Gradio interface
 with gr.Blocks() as demo:
     gr.Markdown("<h1 style='text-align: center;'>Video Editing App</h1>")
     gr.Markdown("Upload a video and specify the edits you want to apply.")
 
     with gr.Row():
         with gr.Column(scale=1):
-            video_input = gr.Video(label="Upload Video")
+            video_input = gr.Video(
+                label="Upload Video",
+                sources=['upload'],
+            )
             edit_instructions = gr.Textbox(label="Edit Instructions")
             extract_button = gr.Button("Submit")
         with gr.Column(scale=1):
